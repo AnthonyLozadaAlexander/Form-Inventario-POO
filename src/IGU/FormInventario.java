91d4594d0c17818ce508af07c2408119c2f7b2bd
zDefcon <antholozadarequelme@gmail.com>
2025-11-20 20:10:34 -0500

    Agrega formulario de inventario
    Se crea la clase FormInventario que extiende JFrame,
    incluyendo la inicialización de componentes para
    el formulario de inventario. Este formulario
    proporciona una interfaz gráfica básica para
    gestionar el inventario.

---
+++ src/IGU/FormInventario.java
@@ -0,0 +1,46 @@
+/*
+ * Created by JFormDesigner on Thu Nov 20 20:03:51 ECT 2025
+ */
+
+package IGU;
+
+import javax.swing.GroupLayout;
+import javax.swing.JFrame;
+
+/**
+ * @author USUARIO
+ */
+public class FormInventario extends JFrame {
+    public FormInventario() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        setTitle("Formulario - Inventario");
+        setLocationRelativeTo(null);
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Educational license - Daniel Sanchez (Anthony Lozada)
+
+        //======== this ========
+        setName("this");
+        var contentPane = getContentPane();
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 804, Short.MAX_VALUE)
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 631, Short.MAX_VALUE)
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Educational license - Daniel Sanchez (Anthony Lozada)
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
