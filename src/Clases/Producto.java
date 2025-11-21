4a601279aa3b99c5595744faa0b6a7aefd3b72fc
zDefcon <antholozadarequelme@gmail.com>
2025-11-20 21:23:42 -0500

    Agrega clase Producto con atributos y métodos básicos
    Se crea la clase Producto que incluye atributos para el nombre y precio, junto con métodos para obtener información sobre el producto. Esta clase facilitará la gestión de productos en el inventario.

---
+++ src/Clases/Producto.java
@@ -0,0 +1,29 @@
+﻿package Clases;
+
+public class Producto {
+
+    private String nombre;
+    private double precio;
+
+    public Producto(String nombre, double precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public Double getPrecio(){
+        return precio;
+    }
+
+    public String mostrarInfo(){
+        return "-------------------------\n" +
+                "Nombre: " + nombre + "\n" +
+                "Precio: " + precio + "$" + "\n"
+                + "-------------------------\n";
+    }
+
+
+}
